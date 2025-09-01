e000ba0b250e230bc473841e23b7f8dc277053e1
Emal <EmalHasitha@proton.me>
2025-09-01 12:13:08 +0530

    fix: handle invalid pawn capture moves

--- src/Game.ts
+++ src/Game.ts
@@ -14,37 +14,10 @@ export class Game {
   private status: GameStatus;
   private activePlayer: PieceColor;
 
-  constructor() {
-    this.board = this.createBoard();
+  constructor(gameBoard: GameBoard) {
+    this.board = gameBoard;
     this.status = null;
     this.activePlayer = "white";
-  }
-
-  private createBoard() {
-    const board: GameBoard = new Array(8).fill(new Array(8).fill(null));
-    board[1] = new Array<ChessPiece>(8).fill("white-pawn");
-    board[6] = new Array<ChessPiece>(8).fill("black-pawn");
-    board[0] = [
-      "white-rook",
-      "white-knight",
-      "white-bishop",
-      "white-queen",
-      "white-king",
-      "white-bishop",
-      "white-knight",
-      "white-rook",
-    ];
-    board[7] = [
-      "black-rook",
-      "black-knight",
-      "black-bishop",
-      "black-queen",
-      "black-king",
-      "black-bishop",
-      "black-knight",
-      "black-rook",
-    ];
-    return board;
   }
 
   private switchPlayerTurn() {
@@ -65,7 +38,7 @@ export class Game {
       case "pawn": {
         const validMoves =
           color === "white"
-            ? VALID_MOVES.pawn
+            ? [...VALID_MOVES.pawn]
             : VALID_MOVES.pawn.map((p) => [p[0], p[1] * -1]);
         if (color === "white" && currentY === 1) validMoves.push([0, 2]);
         if (color === "black" && currentY === 6) validMoves.push([0, -2]);
@@ -75,9 +48,11 @@ export class Game {
           if (!this.isValidMove(posX, posY)) continue;
           const moveType = this.getMoveType(color, posX, posY);
           if (moveType === "invalid") continue;
+          if (moveType === "move" && x !== 0) continue;
 
           const move: Partial<PieceMove> = {};
           move.position = [posX, posY];
+          move.type = moveType;
           const promotion = this.isPawnPromotion(color, posY);
           if (promotion && moveType === "capture") move.type = "all";
           if (promotion && moveType === "move") move.type = "promotion";
@@ -115,4 +90,33 @@ export class Game {
         return posY === 7;
     }
   }
+}
+
+export function createBoard() {
+  const board: GameBoard = Array.from({ length: 8 }, () =>
+    new Array(8).fill(null),
+  );
+  board[1] = new Array<ChessPiece>(8).fill("white-pawn");
+  board[6] = new Array<ChessPiece>(8).fill("black-pawn");
+  board[0] = [
+    "white-rook",
+    "white-knight",
+    "white-bishop",
+    "white-queen",
+    "white-king",
+    "white-bishop",
+    "white-knight",
+    "white-rook",
+  ];
+  board[7] = [
+    "black-rook",
+    "black-knight",
+    "black-bishop",
+    "black-queen",
+    "black-king",
+    "black-bishop",
+    "black-knight",
+    "black-rook",
+  ];
+  return board;
 }
